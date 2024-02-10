9825da3a604a4b6c4330b013d8cd37f3aab4ad4f
Jalgoz <jalgoz95@gmail.com>
2024-02-10 07:25:58 -0400

    Finished lifecycle-app(section 20)

--- 09-lifecycle-app/src/app/products/components/price/price.component.ts
+++ 09-lifecycle-app/src/app/products/components/price/price.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styles: ``,
 })
-export class PriceComponent {}
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+  @Input()
+  public price: number = 0;
+  private interval$?: Subscription; // $ is a convention to indicate that this is an observable
+
+  public ngOnInit(): void {
+    console.log('PriceComponent: ngOnInit');
+    this.interval$ = interval(1000).subscribe((value) => {
+      console.log(`Tick: ${value}`);
+    });
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('PriceComponent: ngOnChanges');
+  }
+
+  public ngOnDestroy(): void {
+    console.log('PriceComponent: ngOnDestroy');
+    // Always unsubscribe from the observable to prevent memory leaks
+    // Exception: If the observable completes on its own, you don't need to unsubscribe
+    // Example the http client observables complete on their own
+    // Also destroy addEventListener
+    this.interval$?.unsubscribe();
+  }
+}
--- 09-lifecycle-app/src/app/products/pages/product-page/product-page.component.ts
+++ 09-lifecycle-app/src/app/products/pages/product-page/product-page.component.ts
@@ -1,8 +1,73 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'products-page',
   templateUrl: './product-page.component.html',
   styles: ``,
 })
-export class ProductPageComponent {}
+export class ProductPageComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  public isProductVisible: boolean = true;
+  public currentPrice: number = 10;
+
+  constructor() {
+    console.log('ProductPageComponent: constructor');
+  }
+
+  public ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('ngOnChanges');
+  }
+
+  public ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  public ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  public ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  public ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  public ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  public ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  public increasePrice(): void {
+    this.currentPrice += 10;
+  }
+}
