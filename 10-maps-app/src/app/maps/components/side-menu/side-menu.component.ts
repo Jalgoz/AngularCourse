f6c0cd0560aef6ccf11a0b0e311896328821e7e0
Jalgoz <jalgoz95@gmail.com>
2024-02-14 16:44:05 -0400

    Add the environments variables to the project(section 21)

--- 10-maps-app/src/app/maps/components/side-menu/side-menu.component.ts
+++ 10-maps-app/src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface MenuItem {
+  title: string;
+  link: string;
+  icon?: string;
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.css']
+  styleUrls: ['./side-menu.component.css'],
 })
 export class SideMenuComponent {
-
+  public menuItems: MenuItem[] = [
+    { title: 'FullScreen', link: '/maps/fullscreen' },
+    { title: 'Zoom', link: '/maps/zoom-range' },
+    { title: 'Markers', link: '/maps/markers' },
+    { title: 'Houses', link: '/maps/properties' },
+  ];
 }
